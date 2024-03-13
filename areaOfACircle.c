77c08b62b661bb58cfd3c4e47360fc8459ab95ae
Rajdp-krmkr <rajdeepkarmakarpc@gmail.com>
2024-03-14 00:00:32 +0530

    Happy

---
+++ areaOfACircle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    // area of a circle formula is = pi*r*r
+
+    float r = 5;
+    float pi = 3.14159265359;
+    printf("area of circle of given radius %f is = %f" , r , pi*r*r);
+    return 0;
+
+}
