29447dca2bae38b533111f4d49b89a955801a54f
Rajdp-krmkr <rajdeepkarmakarpc@gmail.com>
2024-03-13 23:45:23 +0530

    First C Program

---
+++ c1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World 1233\n");
+        float a = 29;
+        float b = 10;
+        float c = a/b;
+        printf("%f" , c);
+
+    return 0;
+}
---
+++ c2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {   
+    int o = 5;
+    int p = 2;
+    float t = 2.0;
+    float r = 2.7;
+    float y;
+    
+    y = o-t;
+    int m;
+    printf("Fuck off");
+    scanf("%d" , &m);
+    printf("%d" , m);
+    // printf("%f" , y);
+
+    float d = o/r;
+    printf("\n%f" , d);
+    return 0;
+
+}
